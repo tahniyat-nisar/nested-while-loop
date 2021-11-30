d695752afc354e242397a35ecf7ec0c83441790f
tahniyat-nisar <93429497+tahniyat-nisar@users.noreply.github.com>
2021-11-30 10:50:11 +0530

    Create diamond " *" pattern.py

---
+++ diamond " *" pattern.py
@@ -0,0 +1,24 @@
+i=1
+while i<=5:
+    b=5-i
+    while b>0:
+        print(" ",end="") #if you put space in end then observe the change
+        b=b-1
+    j=1
+    while j<=i:
+        print("*",end=" ")
+        j=j+1
+    print()
+    i=i+1
+i=4
+while i>=1:
+    b=5-i
+    while b>0:
+        print(" ",end="")#if you put space in end then observe the change
+        b=b-1
+    j=1
+    while j<=i:
+        print("*",end=" ")
+        j=j+1
+    print()
+    i=i-1
